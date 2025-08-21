75b580fe4e75254fbc92ca5f48769eb3aeac13d7
avinash-here <imavinashhere@gmail.com>
2025-08-21 19:28:38 +0530

    Initial push to new repo

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// colabcms/src/App.js
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import TabManager from "./components/TabManager";
+import Login from "./components/Login";
+import { logSheet } from "./utils/sheetUtils";
+import logoutIcon from "./utils/assets/Logoutbtn.png"; // Assuming you have a logout icon
+
+
+function App() {
+  const [user, setUser] = useState(null);
+
+     const handleLogout = async () => {
+      await logSheet({
+     CreatorName: user.fullName,
+     CreatorUserName: user.userId,
+     LogoutTime: new Date().toISOString()
+   });
+     setUser(null);
+   };
+
+  if (!user) return <Login onLogin={setUser} />;
+
+  return (
+    <Container className="text-center mt-4">
+      
+      <TabManager user={user} />
+
+    </Container>
+  );
+}
+
+export default App;
